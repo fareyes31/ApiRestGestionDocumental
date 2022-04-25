9569734782e1b67722aed6a9781ad0b86ec77614
fareyes31 <fareyes31@misena.edu.co>
2022-04-25 16:26:40 -0500

    cambiando idioma de la cantidad minima de caracteres

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -87,7 +87,7 @@ return [
     'min' => [
         'numeric' => 'The :attribute must be at least :min.',
         'file' => 'The :attribute must be at least :min kilobytes.',
-        'string' => 'La :attribute debe tener al menos :min caracteres.',
+        'string' => 'El :attribute debe tener al menos :min caracteres.',
         'array' => 'The :attribute must have at least :min items.',
     ],
     'not_in' => 'The selected :attribute is invalid.',
